9512813ac8b0a52e408f87975e5c6a612b31667b
Kat Lasota <lasota.kat@gmail.com>
2018-08-22 19:13:47 +0200

    17.4

--- script.js
+++ script.js
@@ -1,34 +1,22 @@
 var express = require('express');
-var fs = require('fs');
-var bodyParser = require('body-parser');
-
 var app = express();
-var stringifyFile;
-
-app.use(bodyParser.json());
+app.use(express.static('assets'));
 
 app.get('/', function (req, res) {
-    res.send('Hello world!');
+    res.sendFile('/index.html');
 });
 
-app.get('/getNote', function (req, res) {
-        fs.readFile('./test.json', 'utf8', function(err, data) {
-        if (err) throw err;
-        stringifyFile = data;
-        res.send(data);
-    })
+app.get('/userform', function (req, res) {
+    const response = {
+        first_name: req.query.first_name,
+        last_name: req.query.last_name
+    };
+    res.end(JSON.stringify(response));
 });
 
-app.post('/updateNote/:note', function (req, res) {
-    fs.readFile('./test.json', 'utf8', function(err, data) {
-        if (err) throw err;
-        stringifyFile = data + req.params.note;
-        fs.writeFile('./test.json', stringifyFile, function (err) {
-            if (err) throw err;
-            console.log('file updated');
-        })
+var server = app.listen(3000, 'localhost', function() {
+    var host = server.address().address;
+    var port = server.address().port;
 
-    })
+    console.log('Przykładowa aplikacja nasłuchuje na http://' + host + ':' + port);
 });
-
-app.listen(3000);
